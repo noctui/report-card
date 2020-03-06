da4905dfd59292c3ff2620de03bbc8a05a1f0713
noctui <61877276+noctui@users.noreply.github.com>
2020-03-06 09:43:21 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,134 @@
+$(".pass1").click(function() {
+    $(".art1").css("color" , "#2f9c49");
+    $(".pass1").fadeOut();
+    $(".fail1").fadeOut();
+});
+$(".fail1").click(function() {
+    $(".art2").css("color" , "#d6313f");
+    $(".pass1").fadeOut();
+    $(".fail1").fadeOut();
+
+});
+
+$(".pass2").click(function() {
+    $(".c1").css("color" , "#2f9c49");
+    $(".pass2").fadeOut();
+    $(".fail2").fadeOut();
+});
+$(".fail2").click(function() {
+    $(".c2").css("color" , "#d6313f");
+    $(".pass2").fadeOut();
+    $(".fail2").fadeOut();
+});
+
+$(".pass3").click(function() {
+    $(".ap1").css("color" , "#2f9c49");
+    $(".pass3").fadeOut();
+    $(".fail3").fadeOut();
+});
+$(".fail3").click(function() {
+    $(".ap2").css("color" , "#d6313f");
+    $(".pass3").fadeOut();
+    $(".fail3").fadeOut();
+});
+
+$(".pass4").click(function() {
+    $(".a1").css("color" , "#2f9c49");
+    $(".pass4").fadeOut();
+    $(".fail4").fadeOut();
+});
+$(".fail4").click(function() {
+    $(".a2").css("color" , "#d6313f");
+    $(".pass4").fadeOut();
+    $(".fail4").fadeOut();
+});
+
+$(".pass5").click(function() {
+    $(".g1").css("color" , "#2f9c49");
+    $(".pass5").fadeOut();
+    $(".fail5").fadeOut();
+});
+$(".fail5").click(function() {
+    $(".g2").css("color" , "#d6313f");
+    $(".pass5").fadeOut();
+    $(".fail5").fadeOut();
+});
+
+$(".pass6").click(function() {
+    $(".us1").css("color" , "#2f9c49");
+    $(".pass6").fadeOut();
+    $(".fail6").fadeOut();
+});
+$(".fail6").click(function() {
+    $(".us2").css("color" , "#d6313f");
+    $(".pass6").fadeOut();
+    $(".fail6").fadeOut();
+});
+
+$(".pass7").click(function() {
+    $(".e1").css("color" , "#2f9c49");
+    $(".pass7").fadeOut();
+    $(".fail7").fadeOut();
+});
+$(".fail7").click(function() {
+    $(".e2").css("color" , "#d6313f");
+    $(".pass7").fadeOut();
+    $(".fail7").fadeOut();
+});
+
+$(".rrg").hover(function() {
+    $("body").css("background-color" , "#fffb1c");
+    $(".art1").css("color" , "#fffb1c");
+    $(".art2").css("color" , "#fffb1c");
+    $(".c1").css("color" , "#fffb1c");
+    $(".c2").css("color" , "#fffb1c");
+    $(".ap1").css("color" , "#fffb1c");
+    $(".ap2").css("color" , "#fffb1c");
+    $(".a1").css("color" , "#fffb1c");
+    $(".a2").css("color" , "#fffb1c");
+    $(".g1").css("color" , "#fffb1c");
+    $(".g2").css("color" , "#fffb1c");
+    $(".us1").css("color" , "#fffb1c");
+    $(".us2").css("color" , "#fffb1c");
+    $(".e1").css("color" , "#fffb1c");
+    $(".e2").css("color" , "#fffb1c");
+    
+});
+
+$(".rrg").mouseout(function() {
+    $("body").css("background-color" , "#ffdd57");
+    $(".art1").css("color" , "#ffdd57");
+    $(".art2").css("color" , "#ffdd57");
+    $(".c1").css("color" , "#ffdd57");
+    $(".c2").css("color" , "#ffdd57");
+    $(".ap1").css("color" , "#ffdd57");
+    $(".ap2").css("color" , "#ffdd57");
+    $(".a1").css("color" , "#ffdd57");
+    $(".a2").css("color" , "#ffdd57");
+    $(".g1").css("color" , "#ffdd57");
+    $(".g2").css("color" , "#ffdd57");
+    $(".us1").css("color" , "#ffdd57");
+    $(".us2").css("color" , "#ffdd57");
+    $(".e1").css("color" , "#ffdd57");
+    $(".e2").css("color" , "#ffdd57");
+});
+
+$(".reset").click(function() {
+    $("button").fadeIn();
+    $(".art1").css("color" , "#ffdd57");
+    $(".art2").css("color" , "#ffdd57");
+    $(".c1").css("color" , "#ffdd57");
+    $(".c2").css("color" , "#ffdd57");
+    $(".ap1").css("color" , "#ffdd57");
+    $(".ap2").css("color" , "#ffdd57");
+    $(".a1").css("color" , "#ffdd57");
+    $(".a2").css("color" , "#ffdd57");
+    $(".g1").css("color" , "#ffdd57");
+    $(".g2").css("color" , "#ffdd57");
+    $(".us1").css("color" , "#ffdd57");
+    $(".us2").css("color" , "#ffdd57");
+    $(".e1").css("color" , "#ffdd57");
+    $(".e2").css("color" , "#ffdd57");
+});
+
+
